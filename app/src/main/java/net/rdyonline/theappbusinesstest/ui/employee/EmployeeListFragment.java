3f0c7f14f7cbc053fc1c7886e521a4b8b8ebd3bd
Ben Pearson <ben@rdydev.com>
2015-01-09 09:52:03 +0000

    Project structure

---
+++ app/src/main/java/net/rdyonline/theappbusinesstest/ui/employee/EmployeeListFragment.java
@@ -0,0 +1,9 @@
+package net.rdyonline.theappbusinesstest.ui.employee;
+
+import android.app.Fragment;
+
+/**
+ * Created by rdy on 09/01/15.
+ */
+public class EmployeeListFragment extends Fragment {
+}
